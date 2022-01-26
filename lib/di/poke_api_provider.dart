2436b953f5f1b60c13dcb488f0685ebf38abd2b3
Pulak Kumar Das <pulak1901@gmail.com>
2022-01-26 19:28:10 +0530

    Simplify providers

--- lib/di/poke_api_provider.dart
+++ lib/di/poke_api_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_dex/data/api/poke_api_client.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final pokeApiProvider = Provider<PokeApiClient>((ref) {
-  return PokeApiClient("https://beta.pokeapi.co/graphql/v1beta");
-});
+final pokeApiProvider =
+    Provider((ref) => PokeApiClient("https://beta.pokeapi.co/graphql/v1beta"));
--- lib/di/pokemon_repository_provider.dart
+++ lib/di/pokemon_repository_provider.dart
@@ -2,6 +2,5 @@ import 'package:flutter_dex/data/repository/pokemon_repository_impl.dart';
 import 'package:flutter_dex/di/poke_api_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final pokemonRepositoryProvider = Provider<PokemonRepositoryImpl>((ref) {
-  return PokemonRepositoryImpl(client: ref.read(pokeApiProvider));
-});
+final pokemonRepositoryProvider =
+    Provider((ref) => PokemonRepositoryImpl(client: ref.read(pokeApiProvider)));
