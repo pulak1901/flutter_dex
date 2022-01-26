fa96ca522b58cea2fb7f1210a2619fe6b7db9d39
Pulak Kumar Das <pulak1901@gmail.com>
2022-01-26 19:38:13 +0530

    Create initial gridview

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dex/di/pokemon_repository_provider.dart';
+import 'package:flutter_dex/data/model/pokemon.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dex/di/pokemon_provider.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Material(child: PokemonList()),
+    );
+  }
+}
+
+class PokemonList extends ConsumerWidget {
+  const PokemonList({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SafeArea(
-        child: Material(
-      child: IconButton(
-        icon: Icon(Icons.download),
-        onPressed: () {
-          ref
-              .read(pokemonRepositoryProvider)
-              .getAllPokemon()
-              .then((value) => print(value));
-        },
-      ),
-    ));
+    AsyncValue<List<Pokemon>> pm = ref.watch(pokemonProvider);
+
+    return pm.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, stack) => Text("Error! $err"),
+        data: (pokemon) => GridView.builder(
+              itemCount: pokemon.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
+              itemBuilder: (context, index) => Card(
+                child: Text(pokemon[index].name),
+              ),
+            ));
   }
 }
