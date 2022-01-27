6e142c571300a51a119d702569b62fcf9dc083ad
Pulak Kumar Das <pulak1901@gmail.com>
2022-01-27 17:30:09 +0530

    Simplify details page

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dex/data/model/pokemon.dart';
 import 'package:flutter_dex/data/model/type_colors.dart';
 import 'package:flutter_dex/ui/widgets/pokemon_tile.dart';
-import 'package:flutter_dex/ui/widgets/transition_appbar_delegate.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailsPage extends StatefulWidget {
@@ -19,52 +18,86 @@ class DetailsPage extends StatefulWidget {
 class _DetailsState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
+    final pokemon = widget.pokemon;
+    final _type = pokemon.types.first;
+    final _color = TypeColors.getLighterColorOf(_type, 0.1);
+
     return Scaffold(
       body: DefaultTabController(
         length: 1,
         child: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: TransitionAppBarDelegate(
-                avatar: Container(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 160.0,
+              backgroundColor: _color,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(pokemon.name,
+                    style: TextStyle(
+                        letterSpacing: 1.4, fontWeight: FontWeight.w800)),
+                background: Container(
                   decoration: BoxDecoration(
-                      image: DecorationImage(
-                          isAntiAlias: true,
-                          image: NetworkImage(
-                              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${widget.pokemon.id}.png"),
-                          fit: BoxFit.fitHeight)),
-                ),
-                avatarBg: SvgPicture.asset(
-                  "assets/pokeball.svg",
-                  color: Colors.white24,
-                  width: 156,
-                  fit: BoxFit.cover,
-                ),
-                title: widget.pokemon.name,
-                addiTitle: "#" + widget.pokemon.id.toString().padLeft(3, '0'),
-                subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: PokemonTile.getTiles(widget.pokemon.types)),
-                bgColors: [
-                  TypeColors.getLighterColorOf(widget.pokemon.types[0], 0.3),
-                  TypeColors.colorOf[widget.pokemon.types[0]]!,
-                ],
-                bar: const TabBar(
-                  tabs: [
-                    Tab(text: "About"),
-                  ],
+                    gradient: RadialGradient(colors: [
+                      TypeColors.getLighterColorOf(_type, 0.3),
+                      TypeColors.colorOf[_type]!,
+                    ], radius: 1.3, center: const Alignment(0, 0.2)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, left: 64),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: PokemonTile.getTiles(pokemon.types),
+                        ),
+                      ),
+                      Stack(
+                        children: [
+                          Positioned(
+                            left: 80,
+                            top: 50,
+                            child: SvgPicture.asset(
+                              "assets/pokeball.svg",
+                              color: Colors.white24,
+                              width: 156,
+                              fit: BoxFit.cover,
+                              clipBehavior: Clip.hardEdge,
+                              allowDrawingOutsideViewBox: true,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20, right: 32),
+                            child: Image.network(
+                                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${widget.pokemon.id}.png"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: TabBarView(
-                  children: [
-                    Container(color: Colors.red),
-                  ],
+                height: 20,
+                child: Center(
+                  child: Text('Scroll to see the SliverAppBar in effect.'),
                 ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    color: index.isOdd ? Colors.white : Colors.black12,
+                    height: 100.0,
+                    child: Center(
+                      child: Text('$index', textScaleFactor: 5),
+                    ),
+                  );
+                },
+                childCount: 20,
               ),
             ),
           ],
