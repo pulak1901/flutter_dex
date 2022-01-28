150c2cd32aa5d905126ca72762c6a612ad7c8bf7
Pulak Kumar Das <pulak1901@gmail.com>
2022-01-28 14:02:41 +0530

    Remove _SliverAppBarDelegate

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -209,30 +209,6 @@ class _DetailsState extends ConsumerState<DetailsPage> {
   }
 }
 
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
-  @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      child: _tabBar,
-    );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
-  }
-}
-
 class _FlexibleSpaceBarBg extends StatelessWidget {
   final Pokemon pokemon;
   const _FlexibleSpaceBarBg({Key? key, required this.pokemon})
